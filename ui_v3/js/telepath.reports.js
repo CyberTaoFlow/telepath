86c467a21ebfaa55deccac995bf14d0f74ae9aa8
yehiel <yehiel@hybridsec.com>
2016-06-05 12:54:16 +0300

    creating new Gui rep process

--- ui_v3/js/telepath.reports.js
+++
@@ -1,6 +0,0 @@
-telepath.reports = {
-	init: function () {
-		this.container = $('.tele-panel-reports');
-		this.container.append('<h1 style="padding: 50px; font-size: 24px; font-weight: bold;">Under Construction</h1>');
-	}
-}
