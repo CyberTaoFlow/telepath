1e721ff1bb10f3d7ad630ae07636a03eedce9988
Yehiel Amar <yehiel@hybridsec.com>
2016-08-09 10:57:24 +0300

    sendmail autoconfig added to script

--- configure.sh
+++ configure.sh
@@ -29,6 +29,7 @@ if [ -n "$(which apt-get)" ]; then
 	ln -s ../../mods-available/msgpack.ini ./
 	mv ./msgpack.ini ./20-msgpack.ini
 #	fi
+	sudo sendmailconfig
 
 	luarocks install Lua-cURL
 	luarocks install Lua-cURL --server=https://rocks.moonscript.org/dev
