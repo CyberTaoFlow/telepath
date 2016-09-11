bbf2dc339aa63b8d2af57076df0a0f4b361566bd
roi <roi@hybridsec.com>
2016-09-11 18:43:45 +0300

    Doxygen documentation to the Query.h and geoip.h file.

--- engine/Query.h
+++ engine/Query.h
@@ -6,8 +6,32 @@ using namespace std;
 
 class Query{
 public:
+	//!  Storing the Total Query score.
+	/*!*/
 	double result;
+
+	//!  Defult Constrator.
+	/*!
+		Initializing the object fields with their default values.
+	*/
 	Query();
+
+	//!  Culculating Query Score.
+	/*!
+		Culculating a total query score for a given vector of parameters.
+
+		For Example: \n \n
+
+		Input: \n
+		params = [ \n
+			{score=0.5,length=0,weight=1},       (User-Agent) \n
+			{score=0.75,length=0.5,weight=0.25}, (Host) \n
+			{score=0,length=0,weight=1},         (Content-length) \n
+			{score=0.1,length=0,weight=1}        (Accept)  \n
+		] \n
+		presence = 0.3 \n \n
+		Output : result = ( ( 0.2423<average_score> + 0.0384<average_length> + 0.3<presence> )/3 ) = 0.1935
+	*/
 	Query(vector <Parameter> &,double);
 };
 
--- engine/geoip.h
+++ engine/geoip.h
@@ -5,9 +5,41 @@
 
 using namespace std;
 
+//!  Opening GeoIP Database .
+/*!
+	This function opens the comlete GeoIP version file(/opt/telepath/db/geoip/GeoIPCity.dat) while available. If this file is not exist, Telepath would use the demo GeoIP version file(/opt/telepath/db/geoip/GeoLiteCity.dat).
+
+	\param None
+	\return void
+*/
 void open_geoIP_database();
+
+//!  Deleting GeoIP Obejects + Freeing Memory.
+/*!
+*/
 void delete_geoIP_obj();
+
+//! Getting Country Code Information .
+/*!
+	\param None
+	\return Country code string (2 characters).
+*/
 string getCountry(string&);
+
+//! Getting Coordinate Information .
+/*!
+        \param None
+        \return Coordinate information - latitude & longitude.
+*/
 Coordinate getCoordinate(string & IP);
+
+//! Getting all the Location Information for a given IPv4.
+/*!
+        \param IP a C++ string agrument.
+	\param country a C++ string agrument.
+	\param city a C++ string agrument.
+	\param c_ as a Coordinate object.
+	\return void
+*/
 void getIpInfo(string&,string&,string&,Coordinate&);
 
