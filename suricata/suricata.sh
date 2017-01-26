6e273958d0fbe6b15db34ef6898c8691a7142f22
yehiel amar <yehiel@hybridsec.com>
2017-01-26 10:00:45 +0000

    Update suricata.sh - pfring added to install

--- suricata/suricata.sh
+++ suricata/suricata.sh
@@ -9,7 +9,9 @@ cd /tmp/suricata
 if [ -n "$(which apt-get)" ]; then
         #apt-get --force-yes -y install wget git gcc libpcre3 libpcre3-dbg libpcre3-dev build-essential autoconf automake libtool libpcap-dev libnet1-dev libyaml-0-2 libyaml-dev pkg-config zlib1g zlib1g-dev libcap-ng-dev libcap-ng0
         #make libmagic-dev git libnetfilter-queue-dev libnetfilter-queue1 libnfnetlink-dev libnfnetlink0
-        apt-get --force-yes -y install wget git gcc libpcre3 libpcre3-dbg libpcre3-dev build-essential autoconf automake libtool libpcap-dev libnet1-dev libyaml-0-2 libyaml-dev  zlib1g zlib1g-dev libcap-ng-dev libcap-ng0 libnetfilter-queue-dev libnetfilter-queue1 libnfnetlink-dev libnfnetlink0 make libmagic-dev libjansson-dev libjansson4 pkg-config
+        apt-get --force-yes -y install wget git gcc libpcre3 libpcre3-dbg libpcre3-dev build-essential autoconf automake libtool libpcap-dev libnet1-dev libyaml-0-2 libyaml-dev zlib1g zlib1g-dev libcap-ng-dev libcap-ng0 libnetfilter-queue-dev libnetfilter-queue1 libnfnetlink-dev libnfnetlink0 make libmagic-dev libjansson-dev libjansson4 pkg-config libnuma-dev flex bison subversion
+       #apt-get             -y install              libpcre3 libpcre3-dbg libpcre3-dev build-essential autoconf automake libtool libpcap-dev libnet1-dev libyaml-0-2 libyaml-dev zlib1g zlib1g-dev libcap-ng-dev libcap-ng0                                                                           make libmagic-dev                                       libnuma-dev
+       #apt-get             -y install      git     libpcre3 libpcre3-dbg libpcre3-dev build-essential autoconf automake libtool libpcap-dev libnet1-dev libyaml-0-2 libyaml-dev zlib1g zlib1g-dev libcap-ng-dev libcap-ng0                                                                           make libmagic-dev                            pkg-config libnuma-dev flex bison subversion 
 fi
 
 if [ -n "$(which yum)" ]; then
@@ -23,6 +25,30 @@ cd LuaJIT-2.0.0
 make && make install
 cd ..
 ldconfig
+
+# download pfring
+git clone https://github.com/ntop/PF_RING.git
+cd PF_RING/kernel/
+make && sudo make install
+cd ../userland/lib
+./configure --prefix=/usr/local/pfring && make && sudo make install
+cd ../libpcap
+./configure --prefix=/usr/local/pfring && make && sudo make install
+cd ../tcpdump
+./configure --prefix=/usr/local/pfring && make && sudo make install
+sudo ldconfig
+
+modprobe pf_ring
+
+#wget http://sourceforge.net/projects/ntop/files/PF_RING/PF_RING-6.0.3.tar.gz
+#tar -zxf PF_RING-6.0.3.tar.gz
+#cd PF_RING-6.0.3/
+#make
+
+#cd kernel; make install
+#cd ../userland/lib; make install
+
+#modprobe pf_ring
 
 #rm -rf oisf
 #git clone git://phalanx.openinfosecfoundation.org/oisf.git
@@ -41,7 +67,7 @@ cd "suricata-$VER"
 
 mkdir -p /opt/telepath/suricata/logs
 
-./configure --prefix=/opt/telepath/suricata/ --sysconfdir=/opt/telepath/suricata/ --localstatedir=/opt/telepath/suricata/ --disable-detection --enable-luajit --with-libnss-libraries=/usr/lib --with-libnss-includes=/usr/include/nss/ --with-libnspr-libraries=/usr/lib --with-libluajit-includes=/usr/local/include/luajit-2.0/ --with-libluajit-libraries=/usr/lib/x86_64-linux-gnu/
+./configure --prefix=/opt/telepath/suricata/ --sysconfdir=/opt/telepath/suricata/ --localstatedir=/opt/telepath/suricata/ --disable-detection --enable-luajit --with-libnss-libraries=/usr/lib --with-libnss-includes=/usr/include/nss/ --with-libnspr-libraries=/usr/lib --with-libluajit-includes=/usr/local/include/luajit-2.0/ --with-libluajit-libraries=/usr/lib/x86_64-linux-gnu/ --enable-pfring --with-libpfring-includes=/usr/local/pfring/include --with-libpfring-libraries=/usr/local/pfring/lib
 make
 make install
 ln -s /opt/telepath/suricata/bin/suricata /opt/telepath/suricata/suricata
